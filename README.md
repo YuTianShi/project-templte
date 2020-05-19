adf79e66c858e5244959aa9d9d369c9f3275f7d3
Tye Shi <1508985234@qq.com>
2020-05-19 11:06:41 +0800

    提交

---
+++ README.md
@@ -0,0 +1,1 @@
+springboot项目模板
---
+++ src/main/java/com/tourage/wly/utils/ValidGroups.java
@@ -0,0 +1,33 @@
+package com.tourage.wly.utils;
+
+/**
+ * <p>
+ * Valid分组类型
+ * </p>
+ *
+ * @author zhuYiKang
+ * @since 2019-12-13
+ */
+public interface ValidGroups {
+
+    interface Insert {
+
+    }
+
+    interface Update {
+
+    }
+
+    interface Delete {
+
+    }
+
+    interface Select {
+
+    }
+
+    interface Page {
+
+    }
+
+}
