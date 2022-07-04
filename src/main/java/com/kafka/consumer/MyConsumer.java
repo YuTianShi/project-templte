7cb48b074de910b02ff1e677ed71808761b2499b
施天宇 <1508985234@qq.com>
2022-07-04 18:02:04 +0800

    kafka调试

--- src/main/java/com/kafka/consumer/MyConsumer.java
+++ src/main/java/com/kafka/consumer/MyConsumer.java
@@ -1,7 +1,6 @@
 package com.kafka.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
@@ -22,7 +21,7 @@ public class MyConsumer {
      * },concurrency = "6")
      * //concurrency就是同组下的消费者个数，就是并发消费数，必须小于等于分区总数
      */
-    @KafkaListener(topics = "my-replicated-topic", groupId = "styGroup")
+    //@KafkaListener(topics = "my-replicated-topic", groupId = "styGroup")
     public void listenZhugeGroup(ConsumerRecord<String, String> record, Acknowledgment ack) {
         String value = record.value();
         System.out.println(value);
--- src/main/java/com/kafka/controller/KafkaController.java
+++ src/main/java/com/kafka/controller/KafkaController.java
@@ -1,11 +1,8 @@
 package com.kafka.controller;
 
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 /**
  * KafkaController.
@@ -18,12 +15,12 @@ public class KafkaController {
 
     private final static String TOPIC_NAME = "my-replicated-topic";
 
-    @Resource
-    private KafkaTemplate<String, String> kafkaTemplate;
+    /*@Resource
+    private KafkaTemplate<String, String> kafkaTemplate;*/
 
     @GetMapping("send")
     public void send() {
-        kafkaTemplate.send(TOPIC_NAME, 0, "key", "this is a msg");
+        /*kafkaTemplate.send(TOPIC_NAME, 0, "key", "this is a msg");*/
     }
 
 }
